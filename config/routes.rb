04fa4ef1fb7426719c60bc0bc76d998b0f4ccf8c
Fabian Schulze Wierling <fabian.schuwie@gmail.com>
2019-08-14 18:39:34 +0200

    solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
+# Rails.application.routes.draw do
+
+#   # A visitor can see the list of all restaurants.
+#   get "restaurants", to: 'restaurants#index'
+
+#   # She/He can add a new restaurant, and be redirected to the show view of that new restaurant.
+#   get "restaurants/new", to: 'restaurants#new'
+#   post "restaurants", to: 'restaurants#create'
+
+#   # She/He can see the details of a restaurant, with all the reviews related to the restaurant.
+#   get "restaurants/:id", to: 'restaurants#show'
+
+#   # She/He can add a new review to a restaurant
+#   get "restaurants/:restaurant_id/reviews/new", to: 'reviews#new'
+#   post "restaurants/:restaurant_id/reviews", to: 'reviews#create'
+
+# end
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:create, :new]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'McDonalds',
+    address:      'Kreuzberg, Berlin',
+    phone_number:  'Nice one Yummy yummy.',
+    category:        'chinese',
+  },
+  {
+    name:         'Risa Chicken',
+    address:      'Kreuzberg, Berlin',
+    phone_number:  'Best restaurant Ive been to',
+    category:        'belgian',
+  },
+  {
+    name:         'KFC',
+    address:      'Kreuzberg, Berlin',
+    phone_number:  'was kinda okay.',
+    category:        'french',
+  },
+  {
+    name:         'Dunkin Donuts',
+    address:      'Kreuzberg, Berlin',
+    phone_number:  'next time something else. Waiter was slow.',
+    category:        'belgian',
+  },
+  {
+    name:         'BurgerKing',
+    address:      'Kreuzberg, Berlin',
+    phone_number:  'I found a rat bone in my buger. Worst experience.',
+    category:        'french',
+  }
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
+
